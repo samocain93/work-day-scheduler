8e3aced263ba3356c18f529805610512b590c871
Sam O'Cain <samocain@Sams-MBP.lan>
2022-11-20 20:59:44 -0600

    day displaying at header

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-
-
-
-
-
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+
+
+
 $(document).ready(function () {
 
-  var now = dayjs();
+  var now = dayjs().format("MMMM D, YYYY")
 
-  $("#currentDate").text(dayjs(now))
+  $("#currentDay").text(now);
 
-// console.log(dayjs())
+
+
+
+  // function updateTime() {
+  //   $("#currentDate").text(now);
+  //   setTimeInterval;
+  // };
+
+
+  // function setTimeInterval(){
+  // setInterval(updateTime, 1000);
+  // }
+  
+  // console.log(dayjs())
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
