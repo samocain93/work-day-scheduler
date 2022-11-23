6804c91c2175ca5ba8d4d239d3aa9e0205c73c19
Sam O'Cain <samocain@Sams-MBP.lan>
2022-11-23 16:54:15 -0600

    finished functions and have items getting local storage

--- script.js
+++ script.js
@@ -3,40 +3,19 @@
 // in the html.
 
 
-
-
 $(document).ready(function () {
-
-
-
-//Gets values from textarea and saves to local storage
-  // $(".saveBtn").on("click", function(event) {
-  //   event.preventDefault();
-  //   var value = $(this).siblings(".description").val();
-  //   var time = $(this).parent().attr("id").split("-")[1];
-  //  localStorage.setItem(time, value)
-  // })
-  // $(".saveBtn").on("click", function(event) {
-  //   event.preventDefault();
-  //   var value = $(this).siblings(".description").val();
-  //   var time = $(this).parent().attr("id").split("-")[1];
-  //  localStorage.setItem(time, value)
-  // })
-
-
-
 
   // Var to hold current hour block
   var currentHour = dayjs().hour();
   
-
-
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+  // Gets values from text area and saves to local storage
 
   $(".saveBtn").on("click", function(event) {
     event.preventDefault();
@@ -45,31 +24,16 @@ $(document).ready(function () {
    localStorage.setItem(time, value)
   })
 
-
-
-
-
-
-
-
-
-
-
-
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-  // set up function to compare time id to current hour
+  //Function to compare time id to current hour, change bg colors, and get local storage on load
 
   $(".time-block").each(function() {
-    // var timeBlock = $(this).attr("id").split("-")[1];
+// var to get id element representing time 
+    var timeBlock = $(this).children(".description").attr("id");
+   
+    // sets up variable to get items from local storage and set the value in the time block
+    var blockText = localStorage.getItem(timeBlock);
+    $(this).children(".description").val(blockText);
 
-    // TODO: I want to getthis time block calling the child id without having to run the split function
-    var timeBlock = $(this).attr("id").split("-")[1];
     $(this).removeClass("past present future");
 
     if (timeBlock == currentHour) {
@@ -80,31 +44,6 @@ $(document).ready(function () {
       $(this).addClass("future");
     }
   });
-
-
-
-
-
-
-// localStorage.setItem(time, value)
-
-
-
-
-
-
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-
-
-
-
-
-
-  // TODO: Add code to display the current date in the header of the page.
 
 
   //  var to display current date in header 
